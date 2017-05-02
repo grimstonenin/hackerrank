fb68b4d303ca7d0455a0d4e4df49ab0f892a0afc
andrei.cristea01 <andrei.cristea01@metrosystems.net>
2017-05-02 15:54:32 +0300

    no message

---
+++ src/BalancedBrackets.java
@@ -0,0 +1,67 @@
+/**
+
+ A bracket is considered to be any one of the following characters: (, ), {, }, [, or ].
+
+ Two brackets are considered to be a matched pair if the an opening bracket (i.e., (, [, or {) occurs to the left of a closing bracket (i.e., ), ], or }) of the exact same type. There are three types of matched pairs of brackets: [], {}, and ().
+
+ A matching pair of brackets is not balanced if the set of brackets it encloses are not matched. For example, {[(])} is not balanced because the contents in between { and } are not balanced. The pair of square brackets encloses a single, unbalanced opening bracket, (, and the pair of parentheses encloses a single, unbalanced closing square bracket, ].
+
+ By this logic, we say a sequence of brackets is considered to be balanced if the following conditions are met:
+
+ It contains no unmatched brackets.
+ The subset of brackets enclosed within the confines of a matched pair of brackets is also a matched pair of brackets.
+ Given  strings of brackets, determine whether each sequence of brackets is balanced. If a string is balanced, print YES on a new line; otherwise, print NO on a new line.
+
+ */
+
+import java.util.*;
+
+public class BalancedBrackets {
+
+    public static boolean isBalanced(String expression) {
+
+     Stack<Character> charStack = new Stack<>();
+
+     for (int i =0; i<expression.length(); i++){
+
+         if(expression.charAt(i)=='{' || expression.charAt(i) == '[' || expression.charAt(i) == '('){
+             charStack.push(expression.charAt(i));
+         } else if(expression.charAt(i) == '}' && !charStack.isEmpty()){
+             if(charStack.peek()=='{'){
+                 charStack.pop();
+             } else {
+                 return false;
+             }
+         } else if(expression.charAt(i) == ']' && !charStack.isEmpty()){
+             if(charStack.peek()=='['){
+                 charStack.pop();
+             } else {
+                 return false;
+             }
+         } else if(expression.charAt(i) == ')' && !charStack.isEmpty()){
+             if(charStack.peek()=='('){
+                 charStack.pop();
+             } else {
+                 return false;
+             }
+         }
+
+        }
+
+        if(charStack.isEmpty()) {
+            return true;
+        } else {
+         return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int a0 = 0; a0 < t; a0++) {
+            String expression = in.next();
+            System.out.println( (isBalanced(expression)) ? "YES" : "NO" );
+        }
+    }
+
+}
